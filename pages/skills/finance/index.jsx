70e7096731580cbf529d93750cfdc5db05f2df8f
coreyheckel3 <coreyheckel3@gmail.com>
2024-05-10 18:34:32 -0400

    Remove spaceX files

--- pages/skills/finance/index.jsx
+++ pages/skills/finance/index.jsx
@@ -10,7 +10,26 @@ export default function Finance() {
       </Head>
       <h1>Finance & Economics</h1>
       <ul>
-          
+          <li>Stock Simulations</li>
+          <li>Monte Carlo Simulation</li>
+          <li>Brownian Motion</li>
+          <li>Black Scholes Model</li>
+          <li>Option Pricing</li>
+          <li>Call Option</li>
+          <li>European Options</li>
+          <li>Fair Price</li>
+          <li>Butterfly Spreads</li>
+          <li>Portfolio Optimization</li>
+          <li>ETF Stocks</li>
+          <li>Compound Interest</li>
+          <li>Cash Flow</li>
+          <li>Interest Rates</li>
+          <li>Internal Rate of Return</li>
+          <li>Cost Analysis</li>
+          <li>Tax Analysis</li>
+          <li>Inflation</li>
+          <li>Uncertainty</li>
+          <li>Excel</li>
       </ul>
         <p>
         <Link href={'/skills'}>
